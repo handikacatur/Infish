f432ba285d4fe0eaeff820cb9cdd31a2cdc836c2
Muhammad Haidar <muhammadhaidar862@gmail.com>
2021-06-16 15:28:41 +0700

    revisi nama user login, menu profile to profil akun

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -360,7 +360,7 @@
                   <li class="relative">
                     <button class="font-bold text-gray-900 align-middle rounded-full focus:shadow-outline-purple focus:outline-none" @click="toggleProfileMenu" @keydown.escape="closeProfileMenu" aria-label="Account" aria-haspopup="true">
                       {{-- <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;s=aa3a807e1bbdfd4364d1f449eaa96d82" alt="" aria-hidden="true"> --}}
-                      Akun &nbsp; <i class="fas fa-user w-4 h-4 mr-3"></i>
+                      {{\Auth::user()->name}} &nbsp; <i class="fas fa-user w-4 h-4 mr-3"></i>
                     </button>
                     <template x-if="isProfileMenuOpen">
                       <ul x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" @click.away="closeProfileMenu" @keydown.escape="closeProfileMenu" class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700" aria-label="submenu">
@@ -368,7 +368,7 @@
                         <li class="flex">
                           <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200" href="{{url('edit-profile')}}">
                             <i class="fas fa-user w-4 h-4 mr-3"></i>
-                            <span>Profile</span>
+                            <span>Profil Akun</span>
                           </a>
                         </li>
                         @endrole
@@ -376,7 +376,7 @@
                         <li class="flex">
                           <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200" href="{{url('edit-profile-investor')}}">
                             <i class="fas fa-user w-4 h-4 mr-3"></i>
-                            <span>Profile</span>
+                            <span>Profil Akun</span>
                           </a>
                         </li>
                         @endrole
@@ -384,7 +384,7 @@
                         <li class="flex">
                           <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200" href="{{url('edit-profile-admin')}}">
                             <i class="fas fa-user w-4 h-4 mr-3"></i>
-                            <span>Profile</span>
+                            <span>Profil Akun</span>
                           </a>
                         </li>
                         @endrole
