daa14eb4eb4a77de54de2f5ea13be920ed86cac9
HANDIKA CATUR MAHARDIKA <192410101066@mail.unej.ac.id>
2021-06-17 13:14:09 +0700

    adding dropzonejs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/extern.css') }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{asset('assets/css/dropzone.min.css')}}">
         @isset($extraCSS)
             {{$extraCSS}}
         @endisset
