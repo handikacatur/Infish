9a2216b7f6b4e6d09447f32b6754bcad50899244
Muhammad Haidar <muhammadhaidar862@gmail.com>
2021-06-20 01:51:09 +0700

    revisi notifikasi penjualan: add, edit, delete + penanganan error

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -15,105 +15,125 @@ class SaleController extends Controller
 
     public function index()
     {
-        $newGetPartner = \DB::table('partners')->where('user_id', '=', \Auth::user()->id)->whereNull('deleted_at')->first();
-        $partnerUser = $newGetPartner->id;
-        $listData = \DB::table('transactions')
-        ->select('transactions.id', 'fishes.name', 'transactions.weight', 'transactions.amount', 'transactions.created_at')
-        ->join('fishes', 'transactions.partner_fish_id', '=', 'fishes.id')
-        ->where('transactions.partner_id', '=', $partnerUser)
-        ->whereNull('transactions.deleted_at')
-        ->paginate(5);
+        try {
+            $newGetPartner = \DB::table('partners')->where('user_id', '=', \Auth::user()->id)->whereNull('deleted_at')->first();
+            $partnerUser = $newGetPartner->id;
+            $listData = \DB::table('transactions')
+            ->select('transactions.id', 'fishes.name', 'transactions.weight', 'transactions.amount', 'transactions.created_at')
+            ->join('fishes', 'transactions.partner_fish_id', '=', 'fishes.id')
+            ->where('transactions.partner_id', '=', $partnerUser)
+            ->whereNull('transactions.deleted_at')
+            ->paginate(5);
 
-        $dataFish = \DB::table('partner_fishes')
-        ->select('partner_fishes.id', 'fishes.name')
-        ->join('fishes', 'partner_fishes.fish_id', '=', 'fishes.id')
-        ->where('partner_fishes.partner_id', '=', $partnerUser)
-        ->whereNull('partner_fishes.deleted_at')
-        ->get();
+            $dataFish = \DB::table('partner_fishes')
+            ->select('partner_fishes.id', 'fishes.name')
+            ->join('fishes', 'partner_fishes.fish_id', '=', 'fishes.id')
+            ->where('partner_fishes.partner_id', '=', $partnerUser)
+            ->whereNull('partner_fishes.deleted_at')
+            ->get();
 
-        $penjualanCount = Transaction::select(\DB::raw('COUNT(*) as count'))
-        ->whereYear('created_at', date('Y'))
-        ->whereNull('deleted_at')
-        ->where('partner_id', '=', $partnerUser)
-        ->groupBy(\DB::raw("Month(created_at)"))
-        ->pluck('count');
+            $penjualanCount = Transaction::select(\DB::raw('COUNT(*) as count'))
+            ->whereYear('created_at', date('Y'))
+            ->whereNull('deleted_at')
+            ->where('partner_id', '=', $partnerUser)
+            ->groupBy(\DB::raw("Month(created_at)"))
+            ->pluck('count');
 
-        /* $penjualanCount = Transaction::select(\DB::raw('SUM(amount) as sum'))
-        ->whereYear('created_at', date('Y'))
-        ->whereNull('deleted_at')
-        ->where('partner_id', '=', $partnerUser)
-        ->groupBy(\DB::raw("Month(created_at)"))
-        ->pluck('sum'); */
-        /* dd($penjualanCount); */
+            /* $penjualanCount = Transaction::select(\DB::raw('SUM(amount) as sum'))
+            ->whereYear('created_at', date('Y'))
+            ->whereNull('deleted_at')
+            ->where('partner_id', '=', $partnerUser)
+            ->groupBy(\DB::raw("Month(created_at)"))
+            ->pluck('sum'); */
+            /* dd($penjualanCount); */
 
-        $penjualanBulan = Transaction::select(\DB::raw('Month(created_at) as month'))
-        ->whereYear('created_at', date('Y'))
-        ->whereNull('deleted_at')
-        ->where('partner_id', '=', $partnerUser)
-        ->groupBy(\DB::raw("Month(created_at)"))
-        ->pluck('month');
+            $penjualanBulan = Transaction::select(\DB::raw('Month(created_at) as month'))
+            ->whereYear('created_at', date('Y'))
+            ->whereNull('deleted_at')
+            ->where('partner_id', '=', $partnerUser)
+            ->groupBy(\DB::raw("Month(created_at)"))
+            ->pluck('month');
 
-        $dataPenjualan = array(0,0,0,0,0,0,0,0,0,0,0,0);
-        foreach ($penjualanBulan as $index => $month) {
-            $dataPenjualan[$month] = $penjualanCount[$index];
+            $dataPenjualan = array(0,0,0,0,0,0,0,0,0,0,0,0);
+            foreach ($penjualanBulan as $index => $month) {
+                $dataPenjualan[$month] = $penjualanCount[$index];
+            }
+
+            return view('partner.sale', ['listData' => $listData, 'dataFish' => $dataFish], compact('dataPenjualan'));
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('failed_control', 'Terjadi Kesalahan '.$th);
         }
-
-        return view('partner.sale', ['listData' => $listData, 'dataFish' => $dataFish], compact('dataPenjualan'));
     }
 
     public function save(Request $request)
     {
-        $newGetPartner = \DB::table('partners')->where('user_id', '=', \Auth::user()->id)->whereNull('deleted_at')->first();
-        $partnerUser = $newGetPartner->id;
+        try {
+            $newGetPartner = \DB::table('partners')->where('user_id', '=', \Auth::user()->id)->whereNull('deleted_at')->first();
+            $partnerUser = $newGetPartner->id;
 
-        $request->validate([
-            'weight' => 'required',
-            'amount' => 'required'
-        ]);
+            $request->validate([
+                'weight' => 'required',
+                'amount' => 'required'
+            ]);
 
-        $transaction = new Transaction();
-        $transaction->partner_id = $partnerUser;
-        $transaction->partner_fish_id = $request->fish;
-        $transaction->weight = $request->weight;
-        $transaction->amount = $request->amount;
-        $transaction->save();
+            $transaction = new Transaction();
+            $transaction->partner_id = $partnerUser;
+            $transaction->partner_fish_id = $request->fish;
+            $transaction->weight = $request->weight;
+            $transaction->amount = $request->amount;
+            $transaction->save();
 
-        return redirect('sale');
+            return redirect('sale')->with('saleSuccess', 'Berhasil Menambahkan Penjualan');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('failed_control', 'Terjadi Kesalahan '.$th);
+        }
     }
 
     public function edit(Sale $sale)
     {
-        $newGetPartner = \DB::table('partners')->where('user_id', '=', \Auth::user()->id)->whereNull('deleted_at')->first();
-        $partnerUser = $newGetPartner->id;
-        $dataFish = \DB::table('partner_fishes')
-        ->select('partner_fishes.id', 'fishes.name')
-        ->join('fishes', 'partner_fishes.fish_id', '=', 'fishes.id')
-        ->where('partner_fishes.partner_id', '=', $partnerUser)
-        ->whereNull('partner_fishes.deleted_at')
-        ->get();
+        try {
+            $newGetPartner = \DB::table('partners')->where('user_id', '=', \Auth::user()->id)->whereNull('deleted_at')->first();
+            $partnerUser = $newGetPartner->id;
+            $dataFish = \DB::table('partner_fishes')
+            ->select('partner_fishes.id', 'fishes.name')
+            ->join('fishes', 'partner_fishes.fish_id', '=', 'fishes.id')
+            ->where('partner_fishes.partner_id', '=', $partnerUser)
+            ->whereNull('partner_fishes.deleted_at')
+            ->get();
 
-        return view('partner.sale-edit', ['dataFish' => $dataFish, 'dataSale' => $sale]);
+            return view('partner.sale-edit', ['dataFish' => $dataFish, 'dataSale' => $sale]);
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('failed_control', 'Terjadi Kesalahan '.$th);
+        }
     }
 
     public function put(Request $request, Sale $sale)
     {
-        $request->validate([
-            'weight' => 'required',
-            'amount' => 'required'
-        ]);
-
-        Sale::where('id', $sale->id)->update([
-            'partner_fish_id' => $request->fish,
-            'weight' => $request->weight,
-            'amount' => $request->amount
-        ]);
-
-        return redirect('sale');
+        try {
+            $request->validate([
+                'weight' => 'required',
+                'amount' => 'required'
+            ]);
+    
+            Sale::where('id', $sale->id)->update([
+                'partner_fish_id' => $request->fish,
+                'weight' => $request->weight,
+                'amount' => $request->amount
+            ]);
+    
+            return redirect('sale')->with('saleSuccess', 'Berhasil Memperbarui Ddata Penjualan');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('failed_control', 'Terjadi Kesalahan '.$th);
+        }
     }
 
     public function destroy(Sale $sale)
     {
-        Sale::destroy('id', $sale->id);
-        return redirect('sale');
+        try {
+            Sale::destroy('id', $sale->id);
+            return redirect('sale')->with('saleSuccess', 'Berhasil Menghapus Penjualan');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('failed_control', 'Terjadi Kesalahan '.$th);
+        }
     }
 }
--- resources/views/partner/sale.blade.php
+++ resources/views/partner/sale.blade.php
@@ -14,36 +14,60 @@
         </div>
     </x-slot>
 
+    @if (session('failed_control'))   
+    <div class="mt-10 alert flex flex-row items-center bg-red-200 p-5 rounded border-b-2 border-red-300">
+        <div class="alert-icon flex items-center bg-red-100 border-2 border-red-500 justify-center h-10 w-10 flex-shrink-0 rounded-full">
+            <span class="text-red-500">
+                <svg fill="currentColor"
+                     viewBox="0 0 20 20"
+                     class="h-6 w-6">
+                    <path fill-rule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clip-rule="evenodd"></path>
+                </svg>
+            </span>
+        </div>
+        <div class="alert-content ml-4">
+            <div class="alert-title font-semibold text-lg text-red-800">
+                Terjadi Kesalahan
+            </div>
+            <div class="alert-description text-sm text-red-600">
+                {{session('failed_control')}}
+            </div>
+        </div>
+    </div>
+    @endif
+
     <div class="w-full mt-12 overflow-hidden rounded-lg shadow-sm border-1">
         <div class="w-full overflow-x-auto">
           <table class="w-full whitespace-no-wrap">
             <thead>
               <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                <th class="px-4 py-3">No</th>
-                <th class="px-4 py-3">Tanggal</th>
-                <th class="px-4 py-3">Jenis Ikan</th>
-                <th class="px-4 py-3">Berat</th>
-                <th class="px-4 py-3">Harga</th>
-                <th class="px-4 py-3">Aksi</th>
+                <th class="text-center px-4 py-3">No</th>
+                <th class="text-center px-4 py-3">Tanggal</th>
+                <th class="text-center px-4 py-3">Jenis Ikan</th>
+                <th class="text-center px-4 py-3">Berat</th>
+                <th class="text-center px-4 py-3">Harga</th>
+                <th class="text-center px-4 py-3">Aksi</th>
               </tr>
             </thead>
             <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                 @foreach ($listData as $key => $item)  
                 <tr class="text-gray-700 dark:text-gray-400">
-                    <td class="px-4 py-3">{{$listData->firstItem() + $key}}</td>
-                    <td class="px-4 py-3 text-sm">{{$item->created_at}}</td>
-                    <td class="px-4 py-3 text-sm">{{$item->name}}</td>
-                    <td class="px-4 py-3 text-sm">{{$item->weight}} ton</td>
-                    <td class="px-4 py-3 text-xs">
+                    <td class="text-center px-4 py-3">{{$listData->firstItem() + $key}}</td>
+                    <td class="text-center px-4 py-3 text-sm">{{$item->created_at}}</td>
+                    <td class="text-center px-4 py-3 text-sm">{{$item->name}}</td>
+                    <td class="text-center px-4 py-3 text-sm">{{$item->weight}} ton</td>
+                    <td class="text-center px-4 py-3 text-xs">
                         <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
                             @currency($item->amount)
                         </span>
                     </td>
-                    <td class="px-4 py-3">
-                        <div class="flex items-center space-x-4 text-sm md:flex">
+                    <td class="text-center px-4 py-3">
+                        <div class="flex items-center space-x-4 text-sm md:flex m-auto text-center">
                             <form action="{{url('/edit-sale')}}/{{$item->id}}/change" method="POST" class="m-auto">
                                 @csrf
-                                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+                                <button class="m-auto text-center flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                         <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
                                     </svg>
@@ -52,7 +76,7 @@
                             <form action="{{url('/delete-sale')}}/{{$item->id}}/drop" method="POST" class="m-auto">
                                 @method('delete')
                                 @csrf
-                                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete" onclick="return confirm('Hapus Data ?')">
+                                <button class="m-auto text-center flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete" onclick="return confirm('Hapus Data ?')">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                                     </svg>
@@ -117,8 +141,16 @@
     </div>
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    @if (Session::has('saleSuccess'))
+        <script>
+            swal("Berhasil", "{!! Session::get('saleSuccess') !!}", "success",{
+                button: "OK",
+            })
+        </script>
+    @endif
+
     <script>
         $(document).ready(function() {
             
